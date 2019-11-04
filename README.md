55b341928848f01f095022bd9fe5cc6b92530499
Jiarun Zhou <jiarunzhou@Jiaruns-MacBook-Pro.local>
2019-11-04 10:30:06 -0500

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DI_Project
 This is the repository for the Data Incubator project.
+
+##Project description
+This repository contains the 1-day dietary datatsets in the year of 2013-2014 (DR1IFF_G.XPT), 2014-2015 (DR1IFF_H.XPT), and 2015-2016 (DR1IFF_I.XPT) of National Health and Nutrition Examination Survey (NHNES) obtained from the Centers for Disease Control and Prevention (CDC) website. Analysis are performed to see if there are correlation between the nutrients intake and the eating  
+
+##Package required
+This code is written in python 3.7. In order to run the code, the libraries Numpy, matplotlib, sklearn, seaborn, altair, and Pandas are required. 
+
+##To run the code
+Open Plots.ipynb Jupyter notebook in the environment of python3 and run the code chunk by chunk. Two plots will be generated. 
--- project.py
+++ project.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 import seaborn as sns
-from matplotlib.ticker import StrMethodFormatter
+import altair as alt
 
 df2013_2014 = pd.read_sas('DR1IFF_G.XPT')
 df2014_2015 = pd.read_sas('DR1IFF_H.XPT')
@@ -19,6 +19,7 @@ df2015_2016 = df2015_2016.drop(['WTDRD1', 'WTDR2D', 'DR1DRSTZ', 'DR1EXMER', 'DRD
 df2013_2014 = df2013_2014.query('DRABF == 2')
 df2014_2015 = df2014_2015.query('DRABF == 2')
 df2015_2016 = df2015_2016.query('DRABF == 2')
+
 '''
 df_concat = pd.concat([df2013_2014[['DR1LANG','DR1IPROT','DR1ICARB','DR1IFIBE','DR1ICALC','DR1IMAGN','DR1IIRON','DR1IVARA','DR1IVD','DR1IVC']],\
  df2014_2015[['DR1LANG','DR1IPROT','DR1ICARB','DR1IFIBE','DR1ICALC','DR1IMAGN','DR1IIRON','DR1IVARA','DR1IVD','DR1IVC']], \
@@ -45,23 +46,11 @@ df2013_2014_dinner = df2013_2014.query('DR1_030Z == 3')
 df2014_2015_dinner = df2014_2015.query('DR1_030Z == 3')
 df2015_2016_dinner = df2015_2016.query('DR1_030Z == 3')
 
-print(df2013_2014_dinner.query('DR1IPROT <10'))
-'''
-indexNames = df2013_2014[ df2013_2014['DR1IPROT'] > 100 ].index
-# Delete these row indexes from dataFrame
-df2013_2014.drop(indexNames , inplace=True)
-
-dfbox = pd.DataFrame(df2013_2014[['DR1IPROT','DR1ICARB']], columns=['DR1IPROT','DR1ICARB'])
-axes = dfbox.plot.box()
-axes.set_ylim([0,100])
-plt.show()
+#Reference nutrients intake in the order of 'DR1IPROT','DR1ICARB','DR1IFIBE','DR1ICALC','DR1IMAGN','DR1IIRON','DR1IVARA','DR1IVD','DR1IVC'
+ref = [50.5, 130, 28, 1114.285714, 370, 11.75, 800, 600, 79.375]
 
 
-print(df2013_2014_breakfast[['DR1IPROT','DR1ICARB','DR1IFIBE','DR1ICALC','DR1IMAGN','DR1IIRON','DR1IVARA','DR1IVD','DR1IVC']].mean(),\
-df2013_2014_breakfast[['DR1IPROT','DR1ICARB','DR1IFIBE','DR1ICALC','DR1IMAGN','DR1IIRON','DR1IVARA','DR1IVD','DR1IVC']].std())
-print(df2014_2015_breakfast[['DR1IPROT','DR1ICARB','DR1IFIBE','DR1ICALC','DR1IMAGN','DR1IIRON','DR1IVARA','DR1IVD','DR1IVC']].mean())
-print(df2015_2016_breakfast[['DR1IPROT','DR1ICARB','DR1IFIBE','DR1ICALC','DR1IMAGN','DR1IIRON','DR1IVARA','DR1IVD','DR1IVC']].mean())
-'''
+
 
 
 
